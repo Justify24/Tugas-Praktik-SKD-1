222bd326205c02664789eb1861e914562dfb28fc
Muhammad Farhan <115863849+Justify24@users.noreply.github.com>
2023-09-05 19:15:34 +0700

    update dikit

--- denkripsi1.php
+++ denkripsi1.php
@@ -2,24 +2,24 @@
 // enkripsi1.php
 
 // Define the enkripsi function
-function enkripsi() {
+function denkripsi() {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Get the input data from the form
         $input = $_POST['input'];
 
         // Define the substitution key
         $key = array(
-            'a' => '~', 'i' => '$', 'u' => '^', 'e' => '#', 'o' => '+', 
-            'A' => '!', 'I' => '%', 'U' => '_', 'E' => '=', 'O' => '*'
+            '~' => 'a', '$' => 'i', '^' => 'u', '#' => 'e', '+' => 'o', 
+            '!' => 'A', '%' => 'I', '_' => 'U', '=' => 'E', '*' => 'O'
         );
 
         // Perform the substitution encryption
-        $enkripsi = strtr($input, $key);
+        $denkripsi = strtr($input, $key);
 
         // Display the result
-        echo "Kata yang dienkripsi adalah: $input";
+        echo "Kata yang di denkripsi adalah: $input";
         echo "<br><br>";
-        echo "Hasil enkripsi adalah: $enkripsi";
+        echo "Hasil denkripsi adalah: $denkripsi";
     }
 }
 ?>
--- index.php
+++ index.php
@@ -3,33 +3,68 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enkripsi Form</title>
+    <title>Enkripsi/Deskripsi Form</title>
 
-    <!-- Add Tailwind CSS CDN (you can also install it locally) -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <style>
+    </style>
 </head>
-<body class="bg-black text-white">
+<body class="bg-black text-white flex flex-col min-h-screen">
 
-<?php
-// Include enkripsi1.php to access the enkripsi() function
-require('enkripsi1.php');
+    <!-- Header -->
+    <header class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto">
+            <h1 class="text-2xl font-semibold">Website Enkripsi/Deskripsi</h1>
+        </div>
+    </header>
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $input = $_POST['input'];
-}
-?>
 
-<div class="container mx-auto py-8">
+
+<div class="container mx-auto py-4 flex-grow">
     <form action="" method="POST" class="bg-white p-6 rounded shadow-md">
         <label for="input" class="text-black">Masukkan Data:</label>
-        <input type="text" name="input" id="input" class="border border-black rounded px-2 py-1">
-        <input type="submit" value="Enkripsi" class="bg-black text-white px-4 py-2 rounded mt-4">
+        <div class="container mx-auto">
+            <!-- Tambahkan style="background-color: white;" untuk menghindari transparansi -->
+            <input type="text" name="input" id="input" class="border border-black rounded px-2 py-1 focus:text-black focus:bg-white" placeholder="Masukkan data Anda di sini">
+        </div>
+
+        <!-- Dropdown untuk memilih enkripsi atau dekripsi -->
+        <div class="mt-4">
+            <label for="action" class="text-black">Pilih Tindakan:</label>
+            <select name="action" id="action" class="border border-black rounded px-2 py-1 focus:text-black focus:bg-white">
+                <option value="">Pilih tindakan</option>
+                <option value="enkripsi">Enkripsi</option>
+                <option value="denkripsi">Dekripsi</option>
+            </select>
+        </div>
+
+        <input type="submit" value="Proses" class="bg-black text-white px-4 py-2 rounded mt-4">
     </form>
+</div>
 
     <?php
-    enkripsi(); // Call the enkripsi function to process the form data
+    // Include enkripsi1.php and denkripsi1.php to access the enkripsi() and denkripsi() functions
+    require('enkripsi1.php');
+    require('denkripsi1.php');
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $input = $_POST['input'];
+        $action = $_POST['action'];
+
+        if ($action == 'enkripsi') {
+            $result = enkripsi($input); // Call the enkripsi function
+        } elseif ($action == 'denkripsi') {
+            $result = denkripsi($input); // Call the denkripsi function
+        }
+    }
     ?>
-</div>
+
+    <!-- Footer -->
+    <footer class="bg-gray-800 text-white py-4 mt-auto">
+        <div class="container mx-auto text-center">
+            <p>made by : Muhammad Farhan (V3922033)</p>
+        </div>
+    </footer>
 </body>
 </html>
